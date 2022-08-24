460dbffe17ade52d1e159ceb0865a33ea72a89ef
Hritikrohilla <90026084+TIG3R015@users.noreply.github.com>
2022-08-24 16:59:00 +0530

    Update mgos_ir.c

--- src/mgos_ir.c
+++ src/mgos_ir.c
@@ -65,7 +65,7 @@ static IRAM void irrecv_nec_handler(int pin, void *arg)
       // NO LOG or printf in ISR service routine, or in the handler
       // LOG(LL_DEBUG, ("IRRECV @ %d: %08X", pin, obj->code.dword));
       if (obj->handler) {
-        obj->handler(obj->code.dword,, obj->user_data);
+        obj->handler(obj->code.dword, obj->user_data);
       }
     }
   }
