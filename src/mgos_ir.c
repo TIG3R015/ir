d0cc36cdf49088887ad8620fcdb1838b58ea499a
Hritikrohilla <90026084+TIG3R015@users.noreply.github.com>
2022-08-30 17:15:52 +0530

    Update mgos_ir.c

--- src/mgos_ir.c
+++ src/mgos_ir.c
@@ -89,7 +89,7 @@ static IRAM void irrecv_nec_handler(int pin, void *arg)
   }
 }
 
-struct relays *mgos_ir_response(void)
+struct relays mgos_ir_response(void)
 {
 //   printf("IR:  %X \n", ir_code);
   printf("IR : %d \n", ir_code);
